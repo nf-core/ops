1332b5b5cbf8299208c4db889e19ce8f1ad17d7f
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-19 15:25:50 +0200

    Add example import

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "5.40.0"
     }
   }
 }
 
 provider "github" {
-  # Configuration options
+  token = ""
+  owner = "Applied-Genomics-UTD"
+}
+
+resource "github_repository" "existing_repo" {
+  name        = "repo0088"
+  description = "This is my Github repository"
+  visibility  = "private"
+  auto_init   = true
 }
