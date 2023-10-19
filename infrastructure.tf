5ec55ebcfb404bbc750ec489560589a7ff2cce26
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-19 17:07:52 +0200

    Add infrastructure members as an example

--- infrastructure.tf
+++ infrastructure.tf
@@ -4,9 +4,23 @@
 # }
 
 resource "github_team" "infrastructure" {
-  description = "The best team according to them"
-  # id                        = "8796434"
+  description    = "The best team according to them"
+  id             = "8796434"
   name           = "infrastructure"
   parent_team_id = null
   privacy        = "closed"
+}
+
+resource "github_team_members" "infrastructure" {
+  team_id = github_team.infrastructure.id
+
+  members {
+    username = "mashehu"
+    role     = "maintainer"
+  }
+
+  members {
+    username = "mirpedrol"
+    role     = "maintainer"
+  }
 }
