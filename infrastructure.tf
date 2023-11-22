8ca54e8a7b54d106bb012ccf6384512d101d8128
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-21 18:39:08 -0600

    fix: Remove errors with plan

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,13 +1,7 @@
-# import {
-#   to = github_team.infrastructure
-#   id = "infrastructure"
-# }
-
 resource "github_team" "infrastructure" {
   description    = "The best team according to them"
-  id             = "8796434"
   name           = "infrastructure"
-  parent_team_id = null
+  # parent_team_id = null
   privacy        = "closed"
 }
 
--- modules.tf
+++ modules.tf
@@ -14,30 +14,18 @@ resource "github_repository" "modules" {
   default_branch              = "master"
   delete_branch_on_merge      = false
   description                 = "Repository to host tool-specific module files for the Nextflow DSL2 community!"
-  etag                        = "W/\"cb4e5b8edd311e4a15430550228a029b0a85059d44eb7d839ea464c8adb9e66b\""
-  full_name                   = "nf-core/modules"
-  git_clone_url               = "git://github.com/nf-core/modules.git"
   has_discussions             = false
   has_downloads               = true
   has_issues                  = true
   has_projects                = true
   has_wiki                    = false
   homepage_url                = "https://nf-co.re"
-  html_url                    = "https://github.com/nf-core/modules"
-  http_clone_url              = "https://github.com/nf-core/modules.git"
-  id                          = "modules"
   is_template                 = false
   merge_commit_message        = "PR_TITLE"
   merge_commit_title          = "MERGE_MESSAGE"
   name                        = "modules"
-  node_id                     = "MDEwOlJlcG9zaXRvcnkxOTg5ODIwODM="
-  primary_language            = "Nextflow"
-  private                     = false
-  repo_id                     = 198982083
   squash_merge_commit_message = "COMMIT_MESSAGES"
   squash_merge_commit_title   = "PR_TITLE"
-  ssh_clone_url               = "git@github.com:nf-core/modules.git"
-  svn_url                     = "https://github.com/nf-core/modules"
   topics = [
     "dsl2",
     "modules",
