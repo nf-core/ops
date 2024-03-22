e03dbac15b2dff68905e9c8ea0b2a01d1d717307
Alberto Chiusole <chiusole@seqera.io>
2024-03-22 19:07:37 +0100

    First attempt at managing the nfcore org with pulumi

--- pulumi/github/teams/__main__.py
+++ pulumi/github/teams/__main__.py
@@ -1,26 +1,38 @@
+#!/usr/bin/env python
+
+import yaml
+
 import pulumi
 import pulumi_github as github
 
-# Create a new GitHub team within the nf-core organization
-my_team = github.Team("myTeam",
-    name="my-team",
-    description="My Team Description",
-    privacy="closed",  # Can be 'secret' or 'closed'
-)
+with open('org.yaml') as org_fd:
+    org = yaml.safe_load(org_fd)
 
-# Add a user to the newly created team
-team_membership = github.TeamMembership("teamMembership",
-    team_id=my_team.id,
-    username="example-user",  # Replace with the actual GitHub username
-    role="member",  # Can be 'member' or 'maintainer'
-)
+for repo in org["repositories"]:
+    repo = github.Repository(repo["name"],
+        name=repo["name"],
+        description=repo.get("description", ""),
+        visibility=repo.get("visibility", "private"))
 
-# Associate a repository with the team
-team_repository = github.TeamRepository("teamRepository",
-    team_id=my_team.id,
-    repository="example-repo",  # Replace with the actual repository name
-    permission="push",  # Can be 'pull', 'push', or 'admin'
-)
+for team in org["teams"]:
+    ops = github.Team(team["slug"],
+        name=team["name"],
+        description=team["description"],
+        privacy="closed",
+        opts=pulumi.ResourceOptions(protect=True))
 
-# Export the team slug to access the team on GitHub
-pulumi.export("team_slug", my_team.slug)
+    for user in team["members"]:
+        # Add a user to the newly created team
+        team_membership = github.TeamMembership(f"{team['name']}-{user['name']}",
+            team_id=team["name"],
+            username=user["name"],
+            role=user["role"],
+        )
+
+    for permission in team["permissions"]:
+        # Associate a repository with the team
+        team_repository = github.TeamRepository(f"{team['name']}-{permission['repository']}",
+            team_id=team["name"],
+            repository=permission["repository"],
+            permission=permission["role"],
+        )
