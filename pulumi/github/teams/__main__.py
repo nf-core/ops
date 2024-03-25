1fc892aea11b6f61979c2d076e4c808986d806dc
Alberto Chiusole <chiusole@seqera.io>
2024-03-25 11:26:42 +0100

    Restructure script to manage subteams, add dependencies, more comments in org file

--- pulumi/github/teams/__main__.py
+++ pulumi/github/teams/__main__.py
@@ -5,34 +5,50 @@ import yaml
 import pulumi
 import pulumi_github as github
 
-with open('org.yaml') as org_fd:
-    org = yaml.safe_load(org_fd)
-
-for repo in org["repositories"]:
-    repo = github.Repository(repo["name"],
-        name=repo["name"],
-        description=repo.get("description", ""),
-        visibility=repo.get("visibility", "private"))
-
-for team in org["teams"]:
-    ops = github.Team(team["slug"],
-        name=team["name"],
-        description=team["description"],
-        privacy="closed",
-        opts=pulumi.ResourceOptions(protect=True))
-
-    for user in team["members"]:
-        # Add a user to the newly created team
-        team_membership = github.TeamMembership(f"{team['name']}-{user['name']}",
-            team_id=team["name"],
-            username=user["name"],
-            role=user["role"],
+class Organization:
+    def setup_team(self, team):
+        team_resource = github.Team(team["slug"],
+            name=team["name"],
+            description=team.get("description", ""),
+            privacy="closed",
+            # opts=pulumi.ResourceOptions(protect=True)
         )
 
-    for permission in team["permissions"]:
-        # Associate a repository with the team
-        team_repository = github.TeamRepository(f"{team['name']}-{permission['repository']}",
-            team_id=team["name"],
-            repository=permission["repository"],
-            permission=permission["role"],
-        )
+        for user in team["members"]:
+            # Add a user to the newly created team
+            team_membership = github.TeamMembership(f"{team['name']}-{user['name']}",
+                team_id=team["name"],
+                username=user["name"],
+                role=user.get("role", "member"),
+                opts=pulumi.ResourceOptions(depends_on=[team_resource])
+            )
+
+        for repo in team.get("repositories", []):
+            # Associate a repository with the team
+            team_repository = github.TeamRepository(f"{team['name']}-{repo['name']}",
+                team_id=team["name"],
+                repository=repo["name"],
+                permission=repo.get("permission", "pull"),
+                opts=pulumi.ResourceOptions(depends_on=[self._repos[repo["name"]]])
+            )
+
+        for subteam in team.get("teams", []):
+            self.setup_team(subteam)
+
+    def __init__(self, org_file):
+        self._repos = {}
+        with open(org_file) as org_fd:
+            self._org = yaml.safe_load(org_fd)
+
+        for repo in self._org["repositories"]:
+            self._repos[repo["name"]] = github.Repository(
+                repo["name"],
+                name=repo["name"],
+                description=repo.get("description", ""),
+                visibility=repo.get("visibility", "private")
+            )
+
+        for team in self._org["teams"]:
+            self.setup_team(team)
+
+Organization('org.yaml')
