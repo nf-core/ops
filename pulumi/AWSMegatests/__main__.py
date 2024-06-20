08a128ea9c5dac4b69c5d41399911932af646c98
Edmund Miller <git@edmundmiller.dev>
2024-06-20 14:42:22 -0500

    build: Try running with aws-native

--- pulumi/AWSMegatests/__main__.py
+++ pulumi/AWSMegatests/__main__.py
@@ -1,10 +1,10 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+from pulumi_aws_native import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket("my-bucket")
 
 # Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+pulumi.export("bucket_name", bucket.id)
