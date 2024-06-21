a8330a4ca30d6d76625a868ae5755d08fd897398
Edmund Miller <git@edmundmiller.dev>
2024-06-21 14:49:34 -0500

    style: Run ruff

--- pulumi/github/teams/__main__.py
+++ pulumi/github/teams/__main__.py
@@ -2,21 +2,24 @@ import pulumi
 import pulumi_github as github
 
 # Create a new GitHub team within the nf-core organization
-my_team = github.Team("myTeam",
+my_team = github.Team(
+    "myTeam",
     name="my-team",
     description="My Team Description",
     privacy="closed",  # Can be 'secret' or 'closed'
 )
 
 # Add a user to the newly created team
-team_membership = github.TeamMembership("teamMembership",
+team_membership = github.TeamMembership(
+    "teamMembership",
     team_id=my_team.id,
     username="example-user",  # Replace with the actual GitHub username
     role="member",  # Can be 'member' or 'maintainer'
 )
 
 # Associate a repository with the team
-team_repository = github.TeamRepository("teamRepository",
+team_repository = github.TeamRepository(
+    "teamRepository",
     team_id=my_team.id,
     repository="example-repo",  # Replace with the actual repository name
     permission="push",  # Can be 'pull', 'push', or 'admin'
