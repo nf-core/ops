cc101a13a0a160766337e21aea02ace1b7fe2589
Edmund Miller <git@edmundmiller.dev>
2024-06-21 15:50:42 -0500

    fix: Cut out lifecycle

--- pulumi/test-datasets/__main__.py
+++ pulumi/test-datasets/__main__.py
@@ -27,68 +27,6 @@ awsmegatests_bucket = aws.s3.Bucket(
             max_age_seconds=0,
         )
     ],
-    lifecycle_rules=[
-        aws.s3.BucketLifecycleRuleArgs(
-            abort_incomplete_multipart_upload_days=0,
-            enabled=True,
-            expiration=aws.s3.BucketLifecycleRuleExpirationArgs(
-                date="",
-                days=10,
-                expired_object_delete_marker=False,
-            ),
-            id="Delete_files_older_than_10_days_in_work_directory",
-            noncurrent_version_transitions=[],
-            prefix="work",
-            tags={},
-            transitions=[],
-        ),
-        aws.s3.BucketLifecycleRuleArgs(
-            abort_incomplete_multipart_upload_days=0,
-            enabled=True,
-            expiration=aws.s3.BucketLifecycleRuleExpirationArgs(
-                date="",
-                days=10,
-                expired_object_delete_marker=False,
-            ),
-            id="Delete files older than 10 days in _nextflow folder",
-            noncurrent_version_transitions=[],
-            prefix="_nextflow",
-            tags={},
-            transitions=[],
-        ),
-        aws.s3.BucketLifecycleRuleArgs(
-            abort_incomplete_multipart_upload_days=0,
-            enabled=True,
-            expiration=aws.s3.BucketLifecycleRuleExpirationArgs(
-                date="",
-                days=10,
-                expired_object_delete_marker=False,
-            ),
-            id="Delete files older than 10 days in scratch directory",
-            noncurrent_version_expiration=aws.s3.BucketLifecycleRuleNoncurrentVersionExpirationArgs(
-                days=1,
-            ),
-            noncurrent_version_transitions=[],
-            prefix="scratch",
-            tags={},
-            transitions=[],
-        ),
-        aws.s3.BucketLifecycleRuleArgs(
-            abort_incomplete_multipart_upload_days=0,
-            enabled=True,
-            id="Move_to_intelligent_tier_after_11_days",
-            noncurrent_version_transitions=[],
-            prefix="",
-            tags={},
-            transitions=[
-                aws.s3.BucketLifecycleRuleTransitionArgs(
-                    date="",
-                    days=11,
-                    storage_class="INTELLIGENT_TIERING",
-                )
-            ],
-        ),
-    ],
     request_payer="BucketOwner",
     server_side_encryption_configuration=aws.s3.BucketServerSideEncryptionConfigurationArgs(
         rule=aws.s3.BucketServerSideEncryptionConfigurationRuleArgs(
