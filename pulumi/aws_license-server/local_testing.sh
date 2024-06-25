ac2677ed45c9210a63eef81319942a709219bb52
Edmund Miller <git@edmundmiller.dev>
2024-06-25 14:52:13 -0500

    bump secret locations

--- pulumi/aws_license-server/local_testing.sh
+++ pulumi/aws_license-server/local_testing.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # NOTE If this fails run `op signin` to flip to nf-core
-export SENTIEON_LICSRVR_IP=$(op read "op://Dev/SENTIEON_LICSRVR_IP/IP")
-export SENTIEON_LICSRVR_PORT=$(op read "op://Dev/SENTIEON_LICSRVR_IP/Port")
+export SENTIEON_LICSRVR_IP=$(op read "op://Dev/Sentieon License Server/IP")
+export SENTIEON_LICSRVR_PORT=$(op read "op://Dev/Sentieon License Server/Port")
 
 nextflow secrets set SENTIEON_LICENSE_BASE64 $(echo -n "$SENTIEON_LICSRVR_IP:$SENTIEON_LICSRVR_PORT"| base64 -w 0)
 # nextflow secrets set SENTIEON_AUTH_MECH_BASE64 ${{ secrets.SENTIEON_AUTH_MECH_BASE64 }}
-SENTIEON_ENCRYPTION_KEY=$(op read "op://Dev/SENTIEON_ENCRYPTION_KEY/password")
-SENTIEON_LICENSE_MESSAGE=$(op read "op://Dev/SENTIEON_LICENSE_MESSAGE/password")
+SENTIEON_ENCRYPTION_KEY=$(op read "op://Dev/Sentieon License Server/GitHub Secrets/SENTIEON_ENCRYPTION_KEY")
+SENTIEON_LICENSE_MESSAGE=$(op read "op://Dev/Sentieon License Server/GitHub Secrets/SENTIEON_LICENSE_MESSAGE")
 SENTIEON_AUTH_DATA=$(python3 tests/modules/nf-core/sentieon/license_message.py encrypt --key "$SENTIEON_ENCRYPTION_KEY" --message "$SENTIEON_LICENSE_MESSAGE")
 SENTIEON_AUTH_DATA_BASE64=$(echo -n "$SENTIEON_AUTH_DATA" | base64 -w 0)
 nextflow secrets set SENTIEON_AUTH_DATA_BASE64 $SENTIEON_AUTH_DATA_BASE64
