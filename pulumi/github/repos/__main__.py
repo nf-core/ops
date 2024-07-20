2c2deb666fd54588b436300788e233011cea136c
Edmund Miller <git@edmundmiller.dev>
2024-07-20 18:09:21 -0500

    chore: pulumi import github:index/repository:Repository nf-core-tf modules

--- pulumi/github/repos/__main__.py
+++ pulumi/github/repos/__main__.py
@@ -4,3 +4,41 @@ import yaml
 
 import pulumi
 import pulumi_github as github
+
+
+nf_core_tf = github.Repository(
+    "nf-core-tf",
+    allow_merge_commit=False,
+    allow_rebase_merge=False,
+    allow_squash_merge=False,
+    default_branch="master",
+    description="Repository to host tool-specific module files for the Nextflow DSL2 community!",
+    has_downloads=True,
+    has_issues=True,
+    has_projects=True,
+    homepage_url="https://nf-co.re",
+    merge_commit_message="",
+    merge_commit_title="",
+    name="modules",
+    security_and_analysis=github.RepositorySecurityAndAnalysisArgs(
+        secret_scanning=github.RepositorySecurityAndAnalysisSecretScanningArgs(
+            status="disabled",
+        ),
+        secret_scanning_push_protection=github.RepositorySecurityAndAnalysisSecretScanningPushProtectionArgs(
+            status="disabled",
+        ),
+    ),
+    squash_merge_commit_message="",
+    squash_merge_commit_title="",
+    topics=[
+        "nextflow",
+        "pipelines",
+        "nf-test",
+        "modules",
+        "nf-core",
+        "dsl2",
+        "workflows",
+    ],
+    visibility="public",
+    opts=pulumi.ResourceOptions(protect=True),
+)
