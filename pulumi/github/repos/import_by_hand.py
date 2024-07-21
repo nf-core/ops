83bd8ff9f26dced424e8b790138c6bd01a974e2e
Edmund Miller <git@edmundmiller.dev>
2024-07-20 19:21:26 -0500

    chore: Add two different ways of tackling this
    1. Importing them all by hand, some code duplication and effort, but probably the least likely to blow up
    2. Looping through them all
    We can also start with 1, and then move to 2 once everything is captured in the Pulumi state with 1(which seems like the sane option)

---
+++ pulumi/github/repos/import_by_hand.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import yaml
+
+import pulumi
+import pulumi_github as github
+
+import pipelines.denovotranscript
+import pipelines.meerpipe
+import pipelines.pairgenomealign
+import pipelines.phaseimpute
+import pipelines.reportho
+
+# ...
+
+import core.github
+import core.modules
+
+# ...
+import core.website
---
+++ pulumi/github/repos/loop_example.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import yaml
+
+import pulumi
+import pulumi_github as github
+
+TOPICS = [
+    "nextflow",
+    "pipelines",
+    "nf-test",
+    "modules",
+    "nf-core",
+    "dsl2",
+    "workflows",
+]
+
+alpha_test_pipeline_repos = [
+    "denovotranscript",
+    "meerpipe",
+    "pairgenomealign",
+    "phaseimpute",
+    "reportho",
+]
+
+for pipeline in alpha_test_pipeline_repos:
+    github.Repository(
+        "nf-core-tf",
+        allow_merge_commit=True,
+        allow_rebase_merge=True,
+        allow_squash_merge=True,
+        default_branch="master",
+        description="Alpha test repository for nf-core",
+        has_downloads=True,
+        has_issues=True,
+        has_projects=True,
+        homepage_url=f"https://nf-co.re/{pipeline}",
+        merge_commit_message="",
+        merge_commit_title="",
+        name=pipeline,
+        security_and_analysis=github.RepositorySecurityAndAnalysisArgs(
+            secret_scanning=github.RepositorySecurityAndAnalysisSecretScanningArgs(
+                status="disabled",
+            ),
+            secret_scanning_push_protection=github.RepositorySecurityAndAnalysisSecretScanningPushProtectionArgs(
+                status="disabled",
+            ),
+        ),
+        squash_merge_commit_message="",
+        squash_merge_commit_title="",
+        topics=TOPICS,
+        visibility="public",
+        # NOTE Idk if this will work
+        opts=pulumi.ResourceOptions(protect=True),
+    )
