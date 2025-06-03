0432251158b97de6c970d7579c2ec02da7bcf666
Edmund Miller <edmund.miller@seqera.io>
2025-06-03 15:01:46 -0500

    ci: Add IAM user and policy for CI/CD access to test datasets S3 bucket

--- pulumi/test_datasets/__main__.py
+++ pulumi/test_datasets/__main__.py
@@ -93,5 +93,62 @@ for username in usernames:
         f"{username}-putPolicyAttachment", user=username, policy_arn=policy.arn
     )
 
+# Create a dedicated IAM user for CI/CD access to this bucket
+ci_user = aws.iam.User(
+    "test-datasets-ci-user",
+    name="nf-core-test-datasets-ci",
+    path="/",
+)
+
+# Create access keys for the CI user
+ci_user_access_key = aws.iam.AccessKey(
+    "test-datasets-ci-access-key",
+    user=ci_user.name,
+)
+
+# Create a comprehensive policy for full bucket access
+bucket_access_policy = aws.iam.Policy(
+    "test-datasets-bucket-access-policy",
+    name="nf-core-test-datasets-bucket-access",
+    description="Full access to nf-core-test-datasets S3 bucket",
+    policy=test_datasets_bucket.arn.apply(
+        lambda bucket_arn: f"""{{
+      "Version": "2012-10-17",
+      "Statement": [
+        {{
+          "Effect": "Allow",
+          "Action": [
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+            "s3:DeleteObject",
+            "s3:DeleteObjectVersion",
+            "s3:ListBucket",
+            "s3:ListBucketVersions",
+            "s3:GetBucketLocation",
+            "s3:GetBucketVersioning"
+          ],
+          "Resource": [
+            "{bucket_arn}",
+            "{bucket_arn}/*"
+          ]
+        }}
+      ]
+    }}"""
+    ),
+)
+
+# Attach the comprehensive policy to the CI user
+aws.iam.UserPolicyAttachment(
+    "ci-user-bucket-policy-attachment",
+    user=ci_user.name,
+    policy_arn=bucket_access_policy.arn,
+)
+
 # Export the bucket name
 pulumi.export("bucket_name", test_datasets_bucket.bucket)  # type: ignore[attr-defined]
+# Export the CI user credentials (mark as secret)
+pulumi.export("ci_user_access_key_id", ci_user_access_key.id)
+pulumi.export("ci_user_secret_access_key", pulumi.Output.secret(ci_user_access_key.secret))
+pulumi.export("ci_user_name", ci_user.name)
